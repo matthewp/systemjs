49e1c66ab76db803ee0a7be63eba7145b4e062ee
Justin Meyer <justinbmeyer@gmail.com>
2014-06-21 13:19:30 -0500

    setting back to previous way of loading es6-module-loader

--- dist/system.js
+++ dist/system.js
@@ -1976,31 +1976,38 @@ function __eval(__source, __global, __address, __sourceMap) {
 var __$curScript;
 
 (function(global) {
+  global.upgradeSystemLoader = function() {
+    global.upgradeSystemLoader = undefined;
+    var originalSystem = global.System;
+    global.System = __upgradeSystemLoader(global.System);
+    global.System.clone = function() {
+      return __upgradeSystemLoader(originalSystem);
+    };
+  };
 
-  if (!global.System || !global.LoaderPolyfill) {
+  if (typeof window != 'undefined') {
+    var scripts = document.getElementsByTagName('script');
+    __$curScript = scripts[scripts.length - 1];
 
-    if (typeof window != 'undefined') {
-      var scripts = document.getElementsByTagName('script');
-      __$curScript = scripts[scripts.length - 1];
-
-    
+    if (!global.System || !global.LoaderPolyfill) {
       // determine the current script path as the base path
       var curPath = __$curScript.src;
       var basePath = curPath.substr(0, curPath.lastIndexOf('/') + 1);
       document.write(
         '<' + 'script type="text/javascript" src="' + basePath + 'es6-module-loader.js" data-init="upgradeSystemLoader">' + '<' + '/script>'
       );
-      
-    } else {
-      var es6ModuleLoader = require('es6-module-loader');
-      global.System = es6ModuleLoader.System;
-      global.Loader = es6ModuleLoader.Loader;
-      global.Module = es6ModuleLoader.Module;
-      global.upgradeSystemLoader();
-      module.exports = global.System;
     }
-  } else {
+    else {
+      global.upgradeSystemLoader();
+    }
+  }
+  else {
+    var es6ModuleLoader = require('es6-module-loader');
+    global.System = es6ModuleLoader.System;
+    global.Loader = es6ModuleLoader.Loader;
+    global.Module = es6ModuleLoader.Module;
     global.upgradeSystemLoader();
+    module.exports = global.System;
   }
 })(__$global);
 
